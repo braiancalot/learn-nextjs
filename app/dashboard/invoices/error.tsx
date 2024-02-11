840588d4804016948f8e22a3cfb463e0f1d0698b
braiancalot <calot665@hotmail.com>
2024-02-11 07:22:47 -0300

    feat(ch.14): improving accessbility

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -9,10 +9,6 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  useEffect(() => {
-    console.error(error);
-  }, []);
-
   return (
     <main className="flex h-full flex-col items-center justify-center">
       <h2 className="text-center">Something went wrong!</h2>
